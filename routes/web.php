830d287e284370f87bb6ec88ba5b8e5f1042be5c
Ashraful Afruz <ashrafuzzaman.khan1992@gmail.com>
2021-06-23 22:01:15 +0600

    setting up project frontend and admin part template

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//website
+Route::get('/',['as' => 'website.home', function () {
+    return view('website.index');
+}]);
+
+Route::get('/post', ['as' => 'website.post', function () {
+    return view('website.post');
+}]);
+Route::get('/category',['as' => 'website.category', function () {
+    return view('website.category');
+}]);
+
+
+Route::get('/admins',['as' => 'admin.home', function () {
+    return view('admin.dashboard.index');
+}]);
